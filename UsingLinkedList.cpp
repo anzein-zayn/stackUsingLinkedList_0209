1b27a52b7c9ceb39c4c8a6987c7148239fecb452
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-05-20 10:20:40 +0700

    Membuat prulangan if dengan parameter top = NULL

--- UsingLinkedList.cpp
+++ UsingLinkedList.cpp
@@ -48,7 +48,21 @@ public:
 
     void peek()
     {
-        
+            if(top = NULL)
+            {
+                cout << "List is Empty." <<endl;
+            }
+            else
+            {
+                Node *current = top;
+                while (current != NULL)
+                {
+                    cout << current -> data << " " << endl;
+                    current = current -> next;
+                }
+                cout << endl;
+            }
+
     }
 
 };
